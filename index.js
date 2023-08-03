be35701328d05f2866562aacc8873a2de7540b2b
Hadid Ahmed <hadid.ahmad@optimusfox.com>
2023-08-03 12:56:35 +0500

    First commit

--- index.js
+++ index.js
@@ -14,60 +14,65 @@ app.use(cors());
 // app.get("/testing", async (req, res) => {
 //   return res.send("Success");
 // });
-app.post("/create-applicant", async (req, res) => {
-  try {
-    // const {kycLevel,documentType,documentImage} = req.body
-    externalUserId =
-      "random-JSToken-" + Math.random().toString(36).substr(2, 9);
-    levelName = "basic-kyc-level";
-    console.log("testing ----------------");
-    console.log("object");
-
-    const applicant = await axios(createApplicant(externalUserId, levelName));
-    console.log("============>>>>> Applicant", applicant?.data);
-
-    const applicantid = applicant.data.id;
-    console.log("Applicant id ============>>>>>>>>>>>>", applicantid);
-
-    // res.send(applicant?.data);
-
-    const adddocs = await axios(addDocument(applicantid));
-
-    console.log("============>>>>> adddocs", adddocs?.data);
-
-    res.send(adddocs?.data);
-
-    // const status = await axios(getApplicantStatus(applicantid));
-
-    // console.log("🚀 ~ file: index.js:40 ~ app.post ~ status:", status?.data);
-
-    // res.send({
-    //   applicant: applicant?.data,
-    //   addoc: adddocs?.data,
-    //   status: status?.data,
-    // });
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-// app.post("/create-token", async (req, res) => {
+// app.post("/create-applicant", async (req, res) => {
 //   try {
+//     console.log(req.file);
+//     // const {kycLevel,documentType,documentImage} = req.body
+
 //     externalUserId =
 //       "random-JSToken-" + Math.random().toString(36).substr(2, 9);
 //     levelName = "basic-kyc-level";
+//     // console.log("testing ----------------");
+//     // console.log("object");
 
-//     //   const applicant = await axios(createApplicant(externalUserId, levelName));
+//     const applicant = await axios(createApplicant(externalUserId, levelName));
+//     // console.log("============>>>>> Applicant", applicant?.data);
 
-//     const token = await axios(createAccessToken(externalUserId));
-//     console.log("============>>>>> token", token?.data);
+//     const applicantid = applicant.data.id;
+//     // console.log("Applicant id ============>>>>>>>>>>>>", applicantid);
 
-//     //   res.send(applicant?.data);
-//     res.send(token?.data);
+//     // res.send(applicant?.data);
+
+//     // const adddocs = await axios(addDocument(applicantid));
+
+//     // console.log("============>>>>> adddocs", adddocs?.data);
+
+//     res.send(adddocs?.data);
+
+//     // const status = await axios(getApplicantStatus(applicantid));
+
+//     // console.log("🚀 ~ file: index.js:40 ~ app.post ~ status:", status?.data);
+
+//     // res.send({
+//     //   applicant: applicant?.data,
+//     //   addoc: adddocs?.data,
+//     //   status: status?.data,
+//     // });
 //   } catch (error) {
 //     res.send(error);
 //   }
 // });
+
+app.post("/create-token/:level", async (req, res) => {
+  try {
+    externalUserId =
+      "random-JSToken-" + Math.random().toString(36).substr(2, 9);
+
+    console.log("externalUserId========>>>", externalUserId);
+    levelName = "basic-kyc-level";
+    const { level } = req.params;
+    console.log("LEVEL OUTPUT", level);
+    //   const applicant = await axios(createApplicant(externalUserId, levelName));
+
+    const token = await axios(createAccessToken(externalUserId));
+    console.log("============>>>>> token", token?.data);
+
+    //   res.send(applicant?.data);
+    res.send(token?.data);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 // app.get("/getstatus", async (req, res) => {
 //   try {
